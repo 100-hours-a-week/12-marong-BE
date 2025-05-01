8238fce04122cb8ca15656ddae90ec14f313916c
beviosy <cjstk0021@gmail.com>
2025-05-01 16:43:11 +0900

    Feat: Survey API 예외 처리 추가 (#7)

--- src/main/java/com/ktb/marong/exception/ErrorCode.java
+++ src/main/java/com/ktb/marong/exception/ErrorCode.java
@@ -27,7 +27,11 @@ public enum ErrorCode {
     // OAuth 관련 에러
     INVALID_PROVIDER(400, "provider 값이 올바르지 않습니다."),
     INVALID_KAKAO_CODE(400, "인가 코드가 유효하지 않습니다."),
-    SOCIAL_AUTH_FAILED(401, "소셜 인증에 실패했습니다.");
+    SOCIAL_AUTH_FAILED(401, "소셜 인증에 실패했습니다."),
+
+    // 설문조사 관련 에러
+    SURVEY_NOT_FOUND(404, "설문 정보가 존재하지 않습니다."),
+    INVALID_SURVEY_FORMAT(400, "입력 형식이 잘못되었습니다.");
 
     private final int status;
     private final String message;
