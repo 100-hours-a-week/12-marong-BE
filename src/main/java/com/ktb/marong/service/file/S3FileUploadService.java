63f6748b956188398157b20bebd86a6a282ac663
beviosy <cjstk0021@gmail.com>
2025-05-04 01:34:53 +0900

    Refactor: S3 파일 업로드 서비스 분리 (#8) - FileService → S3FileUploadService 이름 변경 - 프로필 분리로 운영 환경에서만 적용되도록 설정

--- src/main/java/com/ktb/marong/service/file/S3FileUploadService.java
+++ src/main/java/com/ktb/marong/service/file/S3FileUploadService.java
@@ -4,11 +4,10 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.ktb.marong.exception.CustomException;
-import com.ktb.marong.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,20 +17,21 @@ import java.util.UUID;
 
 @Slf4j
 @Service
+@Profile("prod")
 @RequiredArgsConstructor
-public class FileService {
+public class S3FileUploadService implements FileUploadService {
 
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    /**
-     * 이미지 파일 업로드
-     */
-    public String uploadImage(MultipartFile file, String dirName) {
+    @Override
+    public String uploadFile(MultipartFile file, String dirName) throws IOException {
         // 파일 검증
-        validateFile(file);
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("파일이 비어있습니다.");
+        }
 
         // 원본 파일명
         String originalFileName = file.getOriginalFilename();
@@ -52,30 +52,9 @@ public class FileService {
                     new PutObjectRequest(bucket, filePath, inputStream, metadata)
                             .withCannedAcl(CannedAccessControlList.PublicRead)
             );
-        } catch (IOException e) {
-            log.error("파일 업로드 실패: {}", e.getMessage());
-            throw new CustomException(ErrorCode.FILE_UPLOAD_ERROR);
         }
 
         // 업로드된 파일의 S3 URL 반환
         return amazonS3.getUrl(bucket, filePath).toString();
-    }
-
-    /**
-     * 파일 유효성 검사
-     */
-    private void validateFile(MultipartFile file) {
-        if (file.isEmpty()) {
-            throw new CustomException(ErrorCode.EMPTY_FILE);
-        }
-
-        String contentType = file.getContentType();
-        if (contentType == null || (!contentType.startsWith("image/jpeg") && !contentType.startsWith("image/png"))) {
-            throw new CustomException(ErrorCode.INVALID_FILE_FORMAT);
-        }
-
-        if (file.getSize() > 2 * 1024 * 1024) { // 2MB 제한
-            throw new CustomException(ErrorCode.FILE_TOO_LARGE);
-        }
     }
 }
