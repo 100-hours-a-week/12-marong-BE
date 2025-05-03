4c39eb1205ec97ff2c44be9335842a4b00ac9834
beviosy <cjstk0021@gmail.com>
2025-05-04 01:33:15 +0900

    Refactor: S3Config에 @Profile("prod") 어노테이션 추가 (#8) - S3 관련 설정을 운영 환경에서만 사용하도록 프로필 적용

--- src/main/java/com/ktb/marong/config/S3Config.java
+++ src/main/java/com/ktb/marong/config/S3Config.java
@@ -8,8 +8,10 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod") // 프로필이 prod일 때만 이 설정 적용
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
