f5069331a63e895de80c080eecc2f110f08231b7
beviosy <cjstk0021@gmail.com>
2025-05-06 04:48:25 +0900

    Refactor: 게시글 작성 API에서 manittoName 파라미터 제거 (#9)

--- src/main/java/com/ktb/marong/dto/request/feed/PostRequestDto.java
+++ src/main/java/com/ktb/marong/dto/request/feed/PostRequestDto.java
@@ -15,9 +15,6 @@ public class PostRequestDto {
     @NotNull(message = "미션 ID는 필수입니다.")
     private Long missionId;
 
-    @NotBlank(message = "마니또 이름은 필수입니다.")
-    private String manittoName;
-
     @NotBlank(message = "게시글 내용은 필수입니다.")
     @Size(max = 300, message = "게시글 내용은 최대 300자까지 입력 가능합니다.")
     private String content;
