d8f698aa04926b5164ff97f426901215e9f44d34
beviosy <cjstk0021@gmail.com>
2025-05-06 03:28:47 +0900

    Fix: Manitto 리포지토리 반환 타입 변경 (#9) - 마니또 정보 조회 시 NonUniqueResultException 에러 해결 - Optional<Manitto> 반환 타입을 List<Manitto>로 변경

--- src/main/java/com/ktb/marong/repository/ManittoRepository.java
+++ src/main/java/com/ktb/marong/repository/ManittoRepository.java
@@ -4,7 +4,7 @@ import com.ktb.marong.domain.manitto.Manitto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ManittoRepository extends JpaRepository<Manitto, Long> {
@@ -12,5 +12,5 @@ public interface ManittoRepository extends JpaRepository<Manitto, Long> {
     /**
      * 특정 사용자(giver), 그룹, 주차에 해당하는 마니또 정보 조회
      */
-    Optional<Manitto> findByGiverIdAndGroupIdAndWeek(Long giverId, Long groupId, Integer week);
+    List<Manitto> findByGiverIdAndGroupIdAndWeek(Long giverId, Long groupId, Integer week);
 }
