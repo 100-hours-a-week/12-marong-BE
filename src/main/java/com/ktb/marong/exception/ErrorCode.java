1a1b12ccca6003a5d9c6546fad2ed2fbd879e01b
beviosy <cjstk0021@gmail.com>
2025-05-06 03:26:08 +0900

    Feat: 일일 미션 제한 초과 관련 에러코드 추가 (#9)

--- src/main/java/com/ktb/marong/exception/ErrorCode.java
+++ src/main/java/com/ktb/marong/exception/ErrorCode.java
@@ -49,6 +49,7 @@ public enum ErrorCode {
 
     // 미션 관련 에러
     MISSION_NOT_FOUND(404, "미션을 찾을 수 없습니다."),
+    DAILY_MISSION_LIMIT_EXCEEDED(400, "하루에 한 개의 미션만 수행할 수 있습니다."),
 
     // 마니또 관련 에러 추가
     MANITTO_NOT_FOUND(404, "현재 매칭된 마니또가 없습니다."),
