c04f3576153b6eea8a737b0975a8cc34e4feb46b
beviosy <cjstk0021@gmail.com>
2025-05-06 21:34:21 +0900

    Feat: 장소 추천 관련 응답 DTO 구조 정의 (#10)

---
+++ src/main/java/com/ktb/marong/dto/response/recommendation/PlaceRecommendationResponseDto.java
@@ -0,0 +1,27 @@
+package com.ktb.marong.dto.response.recommendation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceRecommendationResponseDto {
+    private List<PlaceDto> restaurants;
+    private List<PlaceDto> cafes;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PlaceDto {
+        private String name;
+        private String category;
+        private String hours;
+    }
+}
